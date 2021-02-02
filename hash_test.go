4c45010b988f22bc33fcec1a0613e2876c82bf1d
Nicolas Thauvin <nico@orgrim.net>
2021-02-02 23:47:13 +0100

    update copyright year

--- hash_test.go
+++ hash_test.go
@@ -1,6 +1,6 @@
 // pg_goback
 //
-// Copyright 2020 Nicolas Thauvin. All rights reserved.
+// Copyright 2020-2021 Nicolas Thauvin. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
