374d3c59d257068e97d89e38dbd8919302c6f5e4
Nicolas Thauvin <nico@orgrim.net>
2021-03-05 18:51:08 +0100

    adjust copyright line

--- hash.go
+++ hash.go
@@ -1,6 +1,6 @@
 // pg_back
 //
-// Copyright 2020-2021 Nicolas Thauvin. All rights reserved.
+// Copyright 2011-2021 Nicolas Thauvin and contributors. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
